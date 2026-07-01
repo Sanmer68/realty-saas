4ea91f2628b50b881451dee380b08459c2b48845
DESKTOP-HEFT53H\Veronica A <lfsm68@gmail.com>
2026-07-01 17:25:16 -0500

    primer commit

---
+++ frontend/src/pages/LandingPublica.tsx
@@ -0,0 +1,4 @@
+const LandingPublica = () => {
+  return <div className="text-white p-8">Landing Pública</div>;
+};
+export default LandingPublica;
---
+++ frontend/src/pages/Perfil.tsx
@@ -0,0 +1,4 @@
+const Perfil = () => {
+  return <div className="text-white p-8">Perfil</div>;
+};
+export default Perfil;
